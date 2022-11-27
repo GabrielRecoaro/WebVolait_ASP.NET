bc057dbb9914a5415c81528671227502ee8ca8d7
brendaberzin <82542387+brendaberzin@users.noreply.github.com>
2022-11-27 15:29:11 -0300

    Update CadastroCupomViewModel.cs

--- WebVolait/ViewModels/CadastroCupomViewModel.cs
+++ WebVolait/ViewModels/CadastroCupomViewModel.cs
@@ -8,12 +8,21 @@ namespace WebVolait.ViewModels
     public class CadastroCupomViewModel
     {
 
+        [Display(Name = "Id do cupom")]
+        [Required(ErrorMessage = "O campo é obrigatório")]
         public int CupomId { get; set; }
 
+        [Display(Name = "Código do cupom")]
+        [Required(ErrorMessage = "O campo é obrigatório")]
         public string Cupomcode { get; set; }
 
+        [Display(Name = "Valor de desconto do cupom")]
+        [Required(ErrorMessage = "O campo é obrigatório")]
         public decimal Valordesconto { get; set; }
 
+        [Display(Name = "Data de validade do cupom")]
+        [Required(ErrorMessage = "O campo é obrigatório")]
+        [DisplayFormat(DataFormatString = "dd/mm/yyyy")]
         public DateTime Cupomvalidade { get; set; }
 
         
